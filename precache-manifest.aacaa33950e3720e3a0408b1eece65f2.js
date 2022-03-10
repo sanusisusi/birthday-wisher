38ae75b27180096e8f40fa5d5f3aed7f45ecbb96
Sanusi Abdulkadir <sanusiabdulkadir00@gmail.com>
2022-03-10 23:47:32 +0100

    Updates

---
+++ precache-manifest.aacaa33950e3720e3a0408b1eece65f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7606e3861f80a444de5a0a21ead3bac2",
+    "url": "/birthday-wisher/index.html"
+  },
+  {
+    "revision": "927197e992460eaefc07",
+    "url": "/birthday-wisher/static/css/main.638ba701.chunk.css"
+  },
+  {
+    "revision": "704c1f0fa1ba0059b722",
+    "url": "/birthday-wisher/static/js/2.37e6f535.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/birthday-wisher/static/js/2.37e6f535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927197e992460eaefc07",
+    "url": "/birthday-wisher/static/js/main.acd1f039.chunk.js"
+  },
+  {
+    "revision": "66f290ee618f9ca64d4e",
+    "url": "/birthday-wisher/static/js/runtime-main.f353cf11.js"
+  }
+]);
